8e4e7eeec1ca64bbb477442e8d750b8498e98d9b
Karol Kuchnio <Karol_Kuchnio@epam.com>
2019-09-19 10:10:05 +0200

    Sample code

---
+++ Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿using Playground.Classes;
+using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
